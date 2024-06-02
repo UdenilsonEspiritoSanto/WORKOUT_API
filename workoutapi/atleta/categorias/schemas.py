766b7307b566c8ad1a11fe87809121e9bbfe6963
UdenilsonEspiritoSanto <udenilson1017@yahoo.com.br>
2024-06-02 18:19:38 -0300

    desafiodioapi

--- workoutapi/atleta/categorias/schemas.py
+++
@@ -1,7 +0,0 @@
-
-from typing import Annotated
-from pydantic import BaseModel, Field
-
-
-class Categoria(BaseModel):
-   nome:Annotated[str,Field(description='nome da categoria',example='Scale',max_length=10)]
